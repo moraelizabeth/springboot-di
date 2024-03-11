ad97c3f9343cb781391886cd93970566d0cd6204
Elizabeth <e.mora@anoki.it>
2024-03-11 15:42:12 +0100

    ProductService

---
+++ src/main/java/com/example/springboot/di/app/springbootdi/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.springboot.di.app.springbootdi.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.springboot.di.app.springbootdi.models.Product;
+import com.example.springboot.di.app.springbootdi.repositories.ProductRepository;
+
+//capa de acceso a datos, trabaja con los datos y la logica del negocio
+public class ProductService {
+
+    private ProductRepository repository = new ProductRepository();
+
+    public List<Product> findAll() {
+        return repository.findAll().stream().map(p -> {
+            Double priceImp = p.getPrice() * 1.25d;
+            p.setPrice(priceImp.longValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(long id) {
+        return repository.findById(id);
+    }
+}
